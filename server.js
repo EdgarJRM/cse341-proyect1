c9a7d5a99a8023284a697f786e60b3e6c7011d99
EdgarJRM <edgarrodriguezm@hotmail.com>
2023-09-23 21:05:56 -0500

    fix descrption put

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
     );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DETETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next();
   });
 app.use('/', require('./routes'));
